873df08776554fd1b9ff50c031c76f9ed525c5df
SharanS2210 <sharans0718@gmail.com>
2024-07-30 20:56:47 +0530

    final

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    String: ["Frontend Developer", "Web Developer", "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+  
